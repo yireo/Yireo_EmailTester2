c957e1d10e0458580c266d374a99d1e63f830c9d
Jisse Reitsma <info@yireo.com>
2018-05-14 10:27:45 +0200

    Pending changes

---
+++ Test/Integration/DiTest.php
@@ -0,0 +1,23 @@
+<?php
+namespace Yireo\EmailTester2\Test\Integration;
+
+use Magento\TestFramework\Helper\Bootstrap;
+use Yireo\EmailTester2\Model\Mailer;
+
+/**
+ * Class DiTest
+ *
+ * @package Yireo\EmailTester2\Test\Integration
+ */
+class DiTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Test whether fetching the mailer through the Object Manager works
+     */
+    public function testMailerTransportBuilder()
+    {
+        /** @var Mailer $mailer */
+        $mailer = Bootstrap::getObjectManager()->create(Mailer::class);
+        $this->assertEquals(get_class($mailer), Mailer::class);
+    }
+}
