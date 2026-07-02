d81c2f7a0089c0f2f2385319cf1f9ce6d6b39cb5
Jisse Reitsma <jisse@yireo.com>
2026-07-02 15:33:51 +0200

    wip: New buttons via form builder

--- Form/FormProvider.php
+++ Form/FormProvider.php
@@ -6,17 +6,29 @@ namespace Yireo\EmailTester2\Form;
 use Loki\AdminComponents\Form\Form;
 use Loki\AdminComponents\Form\FormBuilder;
 use Loki\AdminComponents\Provider\FormProviderInterface;
+use Loki\AdminComponents\Provider\ItemProviderInterface;
+use Magento\Framework\DataObject;
+use Magento\Framework\DataObjectFactory;
 
-class FormProvider implements FormProviderInterface
+class FormProvider implements FormProviderInterface, ItemProviderInterface
 {
     public function __construct(
-        private FormBuilder $formBuilder
+        private FormBuilder $formBuilder,
+        private DataObjectFactory $dataObjectFactory,
     ) {
     }
 
     public function getForm(): Form
     {
         return $this->formBuilder->createForm('emailtester2_form')
+            ->addButton($this->formBuilder->createButton('send', 'Send Email', primary: true))
+            ->addButton($this->formBuilder->createButton('preview', 'Preview Email', primary: true))
+            ->addField($this->formBuilder->createField([
+                'name' => 'mail_from',
+                'type' => 'text',
+                'required' => true,
+                'label' => 'Mail From',
+            ]))
             ->addFieldset(
                 $this->formBuilder->createFieldset('base')
                     ->addField($this->formBuilder->createField([
@@ -28,10 +40,22 @@ class FormProvider implements FormProviderInterface
             );
     }
 
-    public function getData(): array
+    public function getItem(int|string $identifier): DataObject
     {
-        return [
+        return $this->dataObjectFactory->create([
             'mail_from' => 'jisse@yireo.com',
-        ];
+        ]);
+    }
+
+    public function saveItem(DataObject $item): void
+    {
+    }
+
+    public function deleteItem(DataObject $item): void
+    {
+    }
+
+    public function duplicateItem(DataObject $item): void
+    {
     }
 }
